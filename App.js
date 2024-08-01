ab8dac426125263df6f4e50923c3e75456578d58
VictorAndres198 <andresroh98@hotmail.com>
2024-08-01 18:43:40 -0500

    Agregando ReactNativeSafeAreaContext para que la App aparezca debajo del StatusBar, funciona para iOS y Android

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Main } from './components/Main';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Main/>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
@@ -14,7 +18,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ components/Main.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+export function Main() {
+    const insets = useSafeAreaInsets();
+
+  return (    
+    <View style= {{paddingTop: insets.top,
+                  paddingBottom:insets.bottom, 
+                  backgroundColor:'#f87'}}>
+            <Text style={styles.container}>Mes de Año</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+  },
+});
